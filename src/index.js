401f90a270cc5d169ebbc8b9010e90b017a403ca
petriken <petriken82@gmail.com>
2018-10-03 01:08:17 +0300

    advanced-zeros_79

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let a = number;
+  let b = base;
+
+function getSimpleMn(x) {
+    let arr = [];
+
+    for (let i = 2; i <= x; i++) {
+        while (x % i == 0 && x > 1) {
+            arr.push(i);
+            x = x / i;
+        }
+        if (x / i == 1) {
+            arr.push(x);
+        }
+    }
+    g = Math.max.apply(null, arr);
+    return (g);
+}
+
+let res = 0;
+while (a) {
+    a = a / getSimpleMn(b) | 0; // base
+    res += a;
+}
+return res;
 }
