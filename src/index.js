1b4afb4f90ac61a0c3b0e226a9e5a2a3cd312b90
petriken <petriken82@gmail.com>
2018-10-03 22:06:02 +0300

    advanced-zeros_98_test

--- src/index.js
+++ src/index.js
@@ -3,22 +3,31 @@ module.exports = function getZerosCount(number, base) {
   let a = number;
   let b = base;
 
-function getSimpleMn(x) {
+  function getSimpleMn(x) {
     let arr = [];
 
-    for (let i = 2; i <= x; i++) {
-        while (x % i == 0 && x > 1) {
+    for (let i = 2; i <= x; i++) { //разложение на простые множители в массив
+        while (x % i == 0) {
             arr.push(i);
             x = x / i;
         }
-      }
-        return (Math.max.apply(null, arr));
+    }
+
+    let maxFact = (Math.max.apply(null, arr)); // больший множитель
+
+    let count = 0;                       //считаем степень макс. множителя
+    for (var i = 0; i < arr.length; ++i) {
+        if (arr[i] == maxFact)
+            count++;
+    }
+    let fact = [maxFact, count];
+    return fact;
 }
 
-let res = 0;
+let res = 0;                  //считаем нули
 while (a) {
-    a = a / getSimpleMn(b) | 0; // base
-    res += a;
+    a = Math.floor(a / getSimpleMn(b)[0]); 
+    res += a / getSimpleMn(b)[1];
 }
-return res;
+return (Math.floor(res));
 }
